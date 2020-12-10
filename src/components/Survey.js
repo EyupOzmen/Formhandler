0fba7c4face35d5a3d8b0a4bea7cb5e3a25ad486
EyupOzmen <ozmeneyup60@gmail.com>
2020-12-10 22:14:55 +0300

    "validation"

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,14 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import questionJSON from "../data/questions.json";
 import Farewell from "./Farewell";
 
 const Survey = () => {
+  const [brand, setBrand] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [finished, setFinished] = useState(false);
+  const [isRequired, setIsRequired] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const handleRadioChange = (e) => {
+    const { name, value } = e.target;
+    console.log("change", name, value);
+    setBrand([{ name: value }]);
+  };
+  const handleCheckChange = (e) => {
+    const { name, value } = e.target;
+    console.log("change", name, value);
+    let uniqueArray = [...new Set([...categories, `${name}:${value}`])];
+    setCategories(uniqueArray);
+  };
+  console.log("brand", brand);
+  console.log("categories", categories);
 
-  const handleSubmit = () => {
+  const validate = () => {
+    if (brand.length < 1) {
+      setIsRequired(true);
+      setFinished(false);
+    } else {
+      setIsRequired(false);
+      setDisabled(false);
+    }
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
     setFinished(true);
   };
+  useEffect(() => {
+    validate();
+  }, [handleRadioChange, handleCheckChange]);
   return (
     <div>
       {finished ? (
@@ -20,7 +51,11 @@ const Survey = () => {
               return (
                 <div key={question.ID} className="question-container">
                   {question.QuestionType === "MULTISINGLE" && (
-                    <div className="question">
+                    <div
+                      className={`${
+                        isRequired ? "question isRequired" : "question"
+                      }`}
+                    >
                       <p className="question-header">{question.Question}</p>
                       {question.Options.map((item) => (
                         <div
@@ -32,6 +67,7 @@ const Survey = () => {
                             type="radio"
                             name={question.ID}
                             value={item.ID}
+                            onChange={handleRadioChange}
                           />
                           <span className="options-label">{item.Text}</span>
                         </div>
@@ -40,7 +76,11 @@ const Survey = () => {
                   )}
 
                   {question.QuestionType === "MULTIMULTI" && (
-                    <div className="question">
+                    <div
+                      className={`${
+                        question.isRequired ? "question isRequired" : "question"
+                      }`}
+                    >
                       <p className="question-header">{question.Question}</p>
                       {question.Options.map((item) => (
                         <div
@@ -52,6 +92,7 @@ const Survey = () => {
                             type="checkbox"
                             name={question.ID}
                             value={item.ID}
+                            onChange={handleCheckChange}
                           />
                           <span className="options-label">{item.Text}</span>
                         </div>
@@ -62,7 +103,7 @@ const Survey = () => {
               );
             })}
             <div className="question-container">
-              <button className="button" type="submit">
+              <button disabled={disabled} className="button" type="submit">
                 Onayla
               </button>
             </div>
